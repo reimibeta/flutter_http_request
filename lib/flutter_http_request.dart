bea4986df6dcfe7498e4c7045e38edb79d9ad7f1
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-10-20 23:21:05 +0700

    Update flutter_http_request.dart

--- lib/flutter_http_request.dart
+++ lib/flutter_http_request.dart
@@ -3,7 +3,7 @@ library flutter_http_request;
 export 'src/http_service_request.dart' show insert, update, delete, get, post;
 export 'src/http_client_request.dart' show url;
 export 'src/http_parse_request.dart' show parsed;
-export 'src/http_header_request.dart' show headers;
+export 'src/http_header_request.dart' show headers, headerJsonContent, headerJsonContentWithBearerToken, headerUrlEncoded;
 
 class data<T> {
   // getter and setter model
