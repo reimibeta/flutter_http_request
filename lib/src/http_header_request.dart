80f6a299e24ad6066b9bc1333079647067d187ea
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-10-20 23:13:09 +0700

    Update http_header_request.dart

--- lib/src/http_header_request.dart
+++ lib/src/http_header_request.dart
@@ -12,3 +12,15 @@ Map<String, String> headers({Map<String, String> headers, String token}){
     return _httpHeader.headerJsonContentWithBearerToken(token);
   }
 }
+// with be use
+Map<String, String> headerJsonContent(){
+  return _httpHeader.headerJsonContent();
+}
+
+Map<String, String> headerUrlEncoded(){
+  return _httpHeader.headerFormUrlEncoded();
+}
+
+Map<String, String> headerJsonContentWithBearerToken(String token){
+  return _httpHeader.headerJsonContentWithBearerToken(token);
+}
